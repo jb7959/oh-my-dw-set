389119a91caefa1f56d18cdb96f8b558949f3112
jay ahn <jb7959@naver.com>
2022-05-29 20:00:47 +0900

    feat(*): add use case for star schema

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 // The GraphQL schema
+// https://graphql.org/learn/schema/
 const typeDefs = gql`
+  type FactTable {
+    column1: ID
+    column2: String
+    column3: Int
+    column4: Float
+    column5: Boolean  
+  }
+  
+  type FactTable2 {
+    column1: ID
+    column2: String
+    column3: Int
+    column4: Float
+    column5: Boolean  
+  }
+  
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    "Find dimensions from FactTable"
+    dimensions: [FactTable]
+    
+    "Find dimensions from FactTable2"
+    dimensions2: [FactTable2] 
   }
 `;
 
+const FactTable = [
+    {
+        column1: `id-${Date.now()}`,
+        column2: 'string',
+        column3: 3,
+        column4: 4.0,
+        column5: true
+    }
+]
+
 // A map of functions which return data for the schema.
+// Keys of Query Object can be dimensions not defined yet
+// Value of Query Object can be target fact tables
 const resolvers = {
     Query: {
-        hello: () => 'world',
+        dimensions: () => FactTable,
+        dimensions2: () => FactTable
     },
 };
 
